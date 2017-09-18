31cd6085b95931841e63f7dcafa2954fcd45fa9d
Scott Williams <scott.williams@macys.com>
2017-09-18 00:19:45 -0400

    added todo list add and remove capabilities

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+var list = ["Go to the mall", "Ride the ferris wheel", "Eat groceries", "Collect 200 dollars"];
 
-class FirstViewController: UIViewController {
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var todoTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,31 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //populates the list with the correct amount of rows
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count;
+    }
+    
+    //ensures we have up to date view upon navigating to this view
+    override func viewDidAppear(_ animated: Bool) {
+        todoTableView.reloadData();
+    }
+    
+    //fills the row with the necessary data
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "todoTableCell");
+        cell.textLabel?.text = list[indexPath.row];
+        
+        return cell;
+    }
+    
+    //used to remove items by a swipe left
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            list.remove(at: indexPath.row);
+            self.todoTableView.reloadData();
+        }
+    }
 
 }
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var newItemLabel: UILabel!
+    @IBOutlet weak var newItemNameTextField: UITextField!
+    
+    //button press handler
+    @IBAction func addItemButton(_ sender: Any) {
+        self.addItemToList();
+    }
+    
+    func addItemToList() {
+        if(newItemNameTextField.text != "") {
+            list.append(newItemNameTextField.text!);
+        }
+        newItemNameTextField.text = "";
+    }
+    
+    //checks if we entered valid input in the text field
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField.text != "") {
+            self.newItemNameTextField.resignFirstResponder();
+            self.addItemToList();
+            return true;
+        }
+        return false;
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        self.newItemNameTextField.delegate = self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
 
